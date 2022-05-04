2acea97634e3f0d021a395189db6c250fc384946
Stino01 <agostino.carrieri01@gmail.com>
2022-05-04 13:19:20 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "./style/variables.scss";
+  @import "./style/generals.scss";
+</style>
