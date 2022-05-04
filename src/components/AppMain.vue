b73e689a63b366daec253d896a927b0cb1715a02
Stino01 <agostino.carrieri01@gmail.com>
2022-05-04 15:52:45 +0200

    add main component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    
-  </div>
+  <main>
+    <div class="container">
+      <p>Content goes here</p>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -12,4 +14,19 @@ export default {
 
 <style lang="scss">
   @import "../style/variables.scss";
+  @import "../style/generals.scss";
+
+  main {
+    background-color: $bg-black;
+
+    .container {
+      @include my-container;
+      color: white;
+      padding: 3em 0;
+
+      p {
+        font-size: 22px;
+      }
+    }
+  }
 </style>
