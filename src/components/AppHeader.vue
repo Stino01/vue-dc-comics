f5c70048e3df8c1b1cda83c90ebfcdc2b77dac3b
Stino01 <agostino.carrieri01@gmail.com>
2022-05-05 19:08:47 +0200

    esercizio completo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,6 +7,7 @@
           </ul>
         </nav>
     </header>
+    
 </template>
 
 <script>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,17 @@
 <template>
   <main>
+    <div class="img-holder"><img src="../assets/img/jumbotron.jpg" alt="jumbo"></div>
     <div class="container">
-      <p>Content goes here</p>
+      <span>Current Series</span>
+      <div class="box" v-for="(item, index) in product" :key="index">
+        <div class="prod-card">
+            <div class="card-image">
+                <img :src="item.thumb" :alt="item.series">
+            </div>
+            <p>{{item.series}}</p>
+        </div>
+      </div>
+      <div class="button">Load More</div>
     </div>
   </main>
 </template>
@@ -9,6 +19,84 @@
 <script>
 export default {
   name: 'AppMain',
+  data() {
+    return {
+      product: [
+        {
+          thumb: "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+          price: "$19.99",
+          series: "Action Comics",
+          type: "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+          "price": "$3.99",
+          "series": "American Vampire 1976",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+          "price": "$16.99",
+          "series": "Aquaman",
+          "type": "graphic novel"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+          "price": "$2.99",
+          "series": "Batgirl",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+          "price": "$3.99",
+          "series": "Batman",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+          "price": "$2.99",
+          "series": "Batman Beyond",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+          "price": "$3.99",
+          "series": "Batman/Superman",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+          "price": "$4.99",
+          "series": "Batman/Superman Annual",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+          "price": "$5.99",
+          "series": "Batman: The Joker War Zone",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+          "price": "$6.99",
+          "series": "Batman: Three Jokers",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+          "price": "$4.99",
+          "series": "Batman: White Knight Presents: Harley Quinn",
+          "type": "comic book"
+        },
+        {
+          "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+          "price": "$16.99",
+          "series": "Catwoman",
+          "type": "graphic novel"
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -18,13 +106,70 @@ export default {
   main {
     background-color: $bg-black;
 
+    .img-holder {
+      width: 100%;
+      height: 400px;
+      overflow: hidden;
+
+      img {
+        width: 100%;
+        object-fit: cover
+      }
+    }
+
     .container {
       @include my-container;
+      position: relative;
       color: white;
       padding: 3em 0;
+      display: flex;
+      flex-flow: row wrap;
 
-      p {
-        font-size: 22px;
+
+      .box{
+        width: calc((100% / 12) * 2);
+        max-width: calc((100% / 12) * 2);
+        flex: 0 0 auto;
+        
+        .prod-card {
+          padding: 1rem;
+            
+          .card-image {
+            height: 150px;
+            overflow: hidden;
+
+            img {
+              width: 100%;
+            }
+          }
+
+          p {
+            color: white;
+            text-transform: uppercase;
+            padding-top: 1rem;
+          }
+        }
+    }
+
+      span {
+        position: absolute;
+        left: 0;
+        top: -1rem;
+        background-color: $bg-blue;
+        font-size: 20px;
+        font-weight: 600;
+        text-transform: uppercase;
+        padding: 10px;
+      }
+
+      .button {
+        position: absolute;
+        left: 50%;
+        bottom: 1rem;
+        transform: translateX(-50%);
+        background-color: $bg-blue;
+        padding: 5px 30px;
+        text-transform: uppercase;
       }
     }
   }
